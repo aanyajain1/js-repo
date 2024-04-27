dab3ee74a45db4df3a709cc1156121388932ff03
aanyajain1 <109913057+aanyajain1@users.noreply.github.com>
2024-04-27 07:31:28 +0000

    conversion of datatypes

---
+++ 01/2_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all Js code as newer version
+//alert(3+3) we are using nodejs, not browser
+
+ console.log(3+3)
+ //code readability should be high
+
+ let name="Aanya"
+ let age= 18
+ let isLoggedIn= false
+
+ //string =""
+ //boolean=> true or false
+ //null=> standalone value
+ // undefined=> 
+ //symbol =>unique
+
+ //object
+
+ console.log(typeof undefined)
+ 
---
+++ 01/3_conversionOperation.js
@@ -0,0 +1,8 @@
+let score="33"
+
+//const {score}=req.body
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
