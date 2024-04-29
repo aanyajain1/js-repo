497565818f1d238c7852fc3c1bb3a8035cc0f6e0
aanyajain1 <109913057+aanyajain1@users.noreply.github.com>
2024-04-29 10:37:08 +0000

    final

--- 01/3_conversionOperation.js
+++ 01/3_conversionOperation.js
@@ -1,8 +1,23 @@
-let score="33"
+// let score="33"
 
-//const {score}=req.body
-console.log(typeof score)
-console.log(typeof(score))
+// //const {score}=req.body
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+//************************Operations**************************//
+
+// let value=3
+// let negValue= -value
+// console.log(negValue)
+// let str1="hello"
+// let str2="hitesh"
+// let str3=str1+str2
+
+// console.log(str3);
+// console.log("1"+2+2);
+// console.log(2+1+"1");
+// console.log(+true);
+// console.log(2>1)
+console.log("2"===2)
